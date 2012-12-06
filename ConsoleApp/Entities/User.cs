c5a5e462c3c80718127ba02c95baebaffdfc5387
Kunzi Mariano <kunzi.mariano@gmail.com>
2012-12-06 15:39:29 -0300

    First commit

---
+++ ConsoleApp/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson;
+
+namespace ConsoleApp.Entities
+{
+    public class User
+    {
+        public ObjectId Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ConsoleApp.Entities;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.GridFS;
+using MongoDB.Driver.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+
+            var server = client.GetServer();
+            var database = server.GetDatabase("test");
+
+            MongoCollection<User> usrCollection = database.GetCollection<User>("Users");
+
+            var user = new User() { Username = "kun", Email = "some@mail.com" };
+            usrCollection.Insert(user);
+
+            Console.WriteLine(user.Id);
+
+
+
+
+            Console.Read();
+
+        }
+    }
+}
